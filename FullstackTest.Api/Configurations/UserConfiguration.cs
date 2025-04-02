26f8fc2f199648e277bde91ea6547f3fa3326dcf
Julio Bacoli <julio-microlins@hotmail.com>
2025-04-01 23:25:32 -0300

    estrutura inicial + banco de dados

---
+++ FullstackTest.Api/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using FullstackTest.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullstackTest.Api.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FullName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.Username)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(u => u.CreatedAt)
+            .IsRequired();
+    }
+}
---
+++ FullstackTest.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FullstackTest.Api.Configurations;
+using FullstackTest.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullstackTest.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
