ded6ccf4c521bdea87d57eb710d278cd644fb80a
Julio Bacoli <julio-microlins@hotmail.com>
2025-04-06 01:27:07 -0300

    registro de usuario

---
+++ FullstackTest.Front/DTOs/Response/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace FullstackTest.Front.DTOs.Response;
+
+public class AuthResponse
+{
+    public string Token { get; set; }
+    public string Username { get; set; }
+    public Guid UserId { get; set; }
+}
--- FullstackTest.Front/Pages/Register.cshtml.cs
+++ FullstackTest.Front/Pages/Register.cshtml.cs
@@ -1,54 +1,42 @@
-using FullstackTest.Front.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json;
 using System.Text;
 using FullstackTest.Front.DTOs.Response;
-
+using FullstackTest.Front.DTOs.Request;
 
 namespace FullstackTest.Front.Pages;
 
-public class LoginModel : PageModel
+public class RegisterModel : PageModel
 {
     [BindProperty]
-    public LoginRequest LoginRequest { get; set; }
+    public RegisterUserRequest RegisterRequest { get; set; }
 
     public string ErrorMessage { get; set; }
 
-    public void OnGet()
-    {
-    }
-
     public async Task<IActionResult> OnPostAsync()
     {
-        if (!ModelState.IsValid)
-        {
-            ErrorMessage = "Dados inválidos.";
-            return Page();
-        }
-
         using var client = new HttpClient();
         client.BaseAddress = new Uri("https://localhost:44396/");
 
-        var json = JsonSerializer.Serialize(LoginRequest);
+        var json = JsonSerializer.Serialize(RegisterRequest);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-        var response = await client.PostAsync("auth/signin", content);
+        var response = await client.PostAsync("auth/signup", content);
 
         if (!response.IsSuccessStatusCode)
         {
-            ErrorMessage = "Usuário ou senha inválidos.";
+            ErrorMessage = "Falha ao registrar usuário.";
             return Page();
         }
 
         var responseContent = await response.Content.ReadAsStringAsync();
-
-        var result = JsonSerializer.Deserialize<AuthResponse>(
-            responseContent,
+        var result = JsonSerializer.Deserialize<AuthResponse>(responseContent,
             new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
         TempData["Token"] = result?.Token;
 
         return RedirectToPage("/Index");
     }
+
 }
